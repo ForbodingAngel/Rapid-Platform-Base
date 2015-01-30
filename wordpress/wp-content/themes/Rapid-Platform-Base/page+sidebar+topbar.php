6797c98859ed00109370353776ae6b7ce912b0e7
ForbodingAngel <forbodingangel@gmail.com>
2015-01-29 16:01:02 -0800

    Add the ability to have left sidebars Add the ability to choose whether you want the right sidebar to be on top when responsive or not

--- wordpress/wp-content/themes/Rapid-Platform-Base/page+sidebar+topbar.php
+++ wordpress/wp-content/themes/Rapid-Platform-Base/page+sidebar+topbar.php
@@ -6,12 +6,7 @@ Template Name: Page + Sidebar + Top Sidebar (When Responsive)
 get_header(); ?>
 <?php get_template_part( 'sidebar-responsive' ); ?>
 
-	<div id="primary" class="content-area" style="
-/* Inline Styles are bad mmk? But in this case they are necessary because we want our pages to automagically work properly whether they have a sidebar or not */
-float: left;
-max-width: 69%;
-width: 100%;
-">
+	<div id="primary" class="content-area with-right-sidebar">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
--- wordpress/wp-content/themes/Rapid-Platform-Base/sidebar.php
+++ wordpress/wp-content/themes/Rapid-Platform-Base/sidebar.php
@@ -10,13 +10,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<div class="widget-area secondary secondary-nonresponsive" role="complementary" 
-style="
-/* Inline Styles are bad mmk? But in this case they are necessary because we want our pages to automagically work properly whether they have a sidebar or not */
-float: right;
-max-width: 29%;
-width: 100%;
-">
+<div class="widget-area secondary secondary-nonresponsive sidebar-right" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
 </div></div>
