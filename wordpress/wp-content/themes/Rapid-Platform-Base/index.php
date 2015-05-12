3914fe7d0d21972f3640206882b7e600f856bf75
ForbodingAngel <forbodingangel@gmail.com>
2015-05-11 21:08:28 -0700

    Add full support for sticky navigation various other fixes and additions

--- wordpress/wp-content/themes/Rapid-Platform-Base/index.php
+++ wordpress/wp-content/themes/Rapid-Platform-Base/index.php
@@ -12,7 +12,7 @@
  */
 
 get_header(); ?>
-<?php get_template_part( 'sidebar-responsive' ); ?>
+<?php /* get_template_part( 'sidebar-responsive' ); */ ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -43,5 +43,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_template_part( 'sidebar' ); ?>
+<?php /* get_template_part( 'sidebar' ); */ ?>
 <?php get_footer(); ?>
